597d43fe6e5bbab54b2f4da25071f7c11b17eabd
Steven Pears <steven.pears@live.co.uk>
2021-01-30 13:18:55 +0000

    Initial release

---
+++ Slack.NetStandard.AsyncEnumerable/ClientWebSocketExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using Slack.NetStandard.Socket;
+
+namespace Slack.NetStandard.AsyncEnumerable
+{
+    public static class ClientWebSocketExtensions
+    {
+        public static async IAsyncEnumerable<object> SocketModeObjects(this ClientWebSocket client, [EnumeratorCancellation] CancellationToken token, JsonSerializer serializer = default, int bufferSize = 1024)
+        {
+            var mem = new MemoryStream(bufferSize);
+            var memory = new Memory<byte>(new byte[bufferSize]);
+            serializer ??= JsonSerializer.CreateDefault();
+
+            while (!token.IsCancellationRequested)
+            {
+                var result = await client.ReceiveAsync(memory, token);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    break;
+                }
+
+                if (!result.EndOfMessage || mem.Length > 0)
+                {
+                    mem.Write(memory.Span);
+                }
+
+                if (!result.EndOfMessage)
+                {
+                    continue;
+                }
+
+                var msg = mem.Length > 0 ? Encoding.UTF8.GetString(mem.ToArray()) : Encoding.UTF8.GetString(memory.Span);
+
+
+                using var reader = new JsonTextReader(new StreamReader(mem, Encoding.UTF8, false, 1024, true));
+                if (msg.Contains("envelope_id"))
+                {
+                    yield return serializer.Deserialize<Envelope>(reader);
+                }
+                else if (msg.Contains("hello"))
+                {
+                    yield return serializer.Deserialize<Hello>(reader);
+                }
+                else
+                {
+                    yield return serializer.Deserialize<Disconnect>(reader);
+                    yield break;
+                }
+                
+                if (memory.Length > 0)
+                {
+                    mem.SetLength(0);
+                }
+            }
+        }
+    }
+}
---
+++ Slack.NetStandard.AsyncEnumerable/SocketModeClient.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Slack.NetStandard.Socket;
+
+namespace Slack.NetStandard.AsyncEnumerable
+{
+    public abstract class SocketModeClient
+    {
+        public ClientWebSocket WebSocket { get; }
+        public SlackWebApiClient WebClient { get; protected set; }
+
+        protected SocketModeClient() : this(new ClientWebSocket())
+        {
+
+        }
+
+        protected SocketModeClient(ClientWebSocket socket)
+        {
+            WebSocket = socket;
+        }
+
+        public Task ConnectAsync(string appToken, CancellationToken token = default)
+        {
+            return ConnectAsync(new SlackWebApiClient(appToken), token);
+        }
+
+        public virtual async Task ConnectAsync(SlackWebApiClient webClient, CancellationToken token = default)
+        {
+            WebClient = webClient;
+            var connection = await WebClient.Apps.OpenConnection();
+            if (!connection.OK)
+            {
+                throw new InvalidOperationException($"Error from slack: " + connection.Error);
+            }
+
+            await WebSocket.ConnectAsync(new Uri(connection.Url, UriKind.Absolute), token);
+        }
+
+        public virtual async IAsyncEnumerable<Envelope> EnvelopeAsyncEnumerable([EnumeratorCancellation] CancellationToken token)
+        {
+            while (!token.IsCancellationRequested && WebSocket.State == WebSocketState.Open)
+            {
+                Disconnect disconnect = null;
+                await foreach (var socketModeObject in WebSocket.SocketModeObjects(token))
+                    switch (socketModeObject)
+                    {
+                        case Hello hello:
+                            await OnHello(hello, token);
+                            break;
+                        case Envelope env:
+                            yield return env;
+                            break;
+                        case Disconnect dis:
+                            disconnect = dis;
+                            break;
+                    }
+
+                if (disconnect != null && !token.IsCancellationRequested)
+                {
+                    await OnDisconnect(disconnect, token);
+                }
+            }
+        }
+
+        public Task Send(string message, CancellationToken token = default)
+        {
+            return WebSocket.SendAsync(Encoding.UTF8.GetBytes(message), WebSocketMessageType.Text, true, token);
+        }
+
+        protected virtual Task OnHello(Hello hello, CancellationToken token)
+        {
+            return Send("hello", token);
+        }
+
+        protected virtual async Task OnDisconnect(Disconnect connect, CancellationToken token)
+        {
+            await WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disconnect message received from slack",
+                token);
+            if (WebClient != null)
+            {
+                await ConnectAsync(WebClient, token);
+            }
+        }
+    }
+}
